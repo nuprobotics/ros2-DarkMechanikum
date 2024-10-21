8b2e69fc2123d5fc7cc5fd5e1c1bb993658e3ba9
DarkMechanikum <v.shtarev08@gmail.com>
2024-10-21 18:31:09 +0300

    fixed some issues

--- Practice02/src/task01/build/task01/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/workspace/robotics_course/ros2-DarkMechanikum/Practice02/src/task01/install/task01'
