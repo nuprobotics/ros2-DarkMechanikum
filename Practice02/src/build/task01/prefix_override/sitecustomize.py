d71682f5d7be75d965fa3b682fdb65569e8b33a3
DarkMechanikum <v.shtarev08@gmail.com>
2024-10-21 12:27:40 +0300

    did and tested the first task

---
+++ Practice02/src/build/task01/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/workspace/robotics_course/ros2-DarkMechanikum/Practice02/src/install/task01'
---
+++ Practice02/src/task01/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'task01'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='Receiver node example',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'receiver = task01.receiver:main',
+        ],
+    },
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + "/launch", ['launch/task01.launch.py']),  # <-- Include the launch file
+    ],
+)
+
