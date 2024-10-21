96215bcdc8aba20bda3478ca3b6fd9c2e2ad1008
DarkMechanikum <v.shtarev08@gmail.com>
2024-10-21 18:33:57 +0300

    it should launch now

--- Practice02/src/task01/setup.py
+++ Practice02/src/task01/setup.py
@@ -21,7 +21,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + "/launch", ['launch/task01.launch.py']),  # <-- Include the launch file
+        ('share/' + package_name + "/launch", ['launch/task01.launch']),  # <-- Include the launch file
     ],
 )
 
